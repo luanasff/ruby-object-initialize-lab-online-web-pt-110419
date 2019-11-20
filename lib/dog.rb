8a31c47ce020f16dd934fb1f1eb13c7191aaa44e
Luana Faria Lane <luanasfff@gmail.com>
2019-11-20 22:53:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,15 @@
-class Dog 
-  
+
+  class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+end
